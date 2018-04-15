ce112f832249f88bdfb5c1de0f5e06dbc5c40723
xinyoulee <s106061543@m106.nthu.edu.tw>
2018-04-15 22:33:51 +0800

    added crc.h and crc.cpp for function declation and implementation.

---
+++ crc.h
@@ -0,0 +1,34 @@
+ ///////////////////////////////////////////////////////////////////////////////
+ // file: crc.h
+ // Project: ecclab
+ // Description: 
+ //
+ // Change history: Apr. 15, 2018 - file created.
+ // 				
+ //
+ ///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <bitset>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <math.h>
+#include <time.h>
+
+using namespace std;
+
+
+
+
+
+
+
